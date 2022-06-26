e89e260565472219005e9eef1f2cf32088d5aa5e
Aditya Kumar <kumaraditya36912@gmail.com>
2022-06-26 16:40:01 +0530

    comment

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoose= require("mongoose");
 mongoose.connect('mongodb://127.0.0.1:27017/fruitsDB', {useNewUrlParser: true,  useUnifiedTopology: true});
 var connection = mongoose.connection;
 
-//creatin a schema
+//creating a schema
 const FruitsSchema= mongoose.Schema({
     name: {
         type:String,
